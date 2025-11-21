027b30bd285b310f726e295954310125a7f5cae6
acrighthere <rodionovmaksprog@mail.ru>
2025-11-22 00:20:00 +0300

    Фикс

--- src/main/kotlin/ru/joutak/minigames/listener/PlayerJoinListener.kt
+++ src/main/kotlin/ru/joutak/minigames/listener/PlayerJoinListener.kt
@@ -6,17 +6,25 @@ import net.kyori.adventure.text.format.NamedTextColor
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
+import ru.joutak.minigames.MiniGamesCore
 
 object PlayerJoinListener : Listener {
 
     @EventHandler
     fun onJoin(event: PlayerJoinEvent) {
         val player = event.player
-        player.sendMessage(
-            Component.text("Основные команды:\n", NamedTextColor.YELLOW)
-                .append(Component.text("/ready - встать в очередь\n", NamedTextColor.YELLOW))
-                .append(Component.text("/unready - выйти из очереди\n", NamedTextColor.YELLOW))
-                .append(Component.text("/lobby - вернуться в лобби миниигр", NamedTextColor.YELLOW))
+        player.scheduler.runDelayed(
+            MiniGamesCore.plugin,
+            { _ ->
+                player.sendMessage(
+                    Component.text("Основные команды:\n", NamedTextColor.YELLOW)
+                        .append(Component.text("/ready - встать в очередь\n", NamedTextColor.YELLOW))
+                        .append(Component.text("/unready - выйти из очереди\n", NamedTextColor.YELLOW))
+                        .append(Component.text("/lobby - вернуться в лобби миниигр", NamedTextColor.YELLOW))
+                )
+            },
+            null,      // retired (можно оставить null)
+            20L        // задержка в тиках
         )
     }
 }
