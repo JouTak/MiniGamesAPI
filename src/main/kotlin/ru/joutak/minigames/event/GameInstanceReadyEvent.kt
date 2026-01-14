76f6f32a21fd5626fffc6cd8251d9ac0e0e029f6
Denis K. Berman (EnderDissa) <enderdissa@gmail.com>
2026-01-14 03:04:26 +0300

    fixed log spamming

--- src/main/kotlin/ru/joutak/minigames/event/GameInstanceReadyEvent.kt
+++ src/main/kotlin/ru/joutak/minigames/event/GameInstanceReadyEvent.kt
@@ -6,10 +6,18 @@ import ru.joutak.minigames.domain.GameInstance
 
 class GameInstanceReadyEvent(val instance: GameInstance) : Event() {
     companion object {
-        private val handlers = HandlerList()
+        /**
+         * Bukkit event system expects a static handler list.
+         *
+         * IMPORTANT: do not name this backing field `handlers`.
+         * In Kotlin such a property can generate a JVM accessor named `getHandlers()`,
+         * which may clash with Bukkit's Event#getHandlers and lead to recursion/StackOverflow.
+         */
+        private val HANDLER_LIST = HandlerList()
+
         @JvmStatic
-        fun getHandlerList() = handlers
+        fun getHandlerList(): HandlerList = HANDLER_LIST
     }
 
-    override fun getHandlers(): HandlerList = handlers
+    override fun getHandlers(): HandlerList = HANDLER_LIST
 }
